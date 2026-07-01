aec3845f322bc5f16a1cd34c394c631a6bb6a5ef
SnoozymNo <r1126262@gmail.com>
2026-07-01 14:34:44 +0200

    MainMenuWorkingWithSinglePlayer

--- Source/DUCKDUCK/MainMenuWidget.cpp
+++ Source/DUCKDUCK/MainMenuWidget.cpp
@@ -2,4 +2,90 @@
 
 
 #include "MainMenuWidget.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Kismet/GameplayStatics.h"
 
+bool UMainMenuWidget::Initialize() {
+    if (!Super::Initialize())
+    {
+        return false;
+    }
+    if (!ensure(SingleButton)) {
+
+        return false;
+    }
+    if (!ensure(LocalButton)) {
+        return false;
+    }
+    if (!ensure(OnlineButton)) {
+        return false;
+    }
+    if (!ensure(QuitButton)) {
+        return false;
+    }
+    if (!ensure(HostButton)) {
+        return false;
+    }
+    if (!ensure(JoinButton)) {
+        return false;
+    }
+    if (!ensure(BackButton)) {
+        return false;
+    }
+    if (!ensure(MenuSwitcher)) {
+        return false;
+    }
+
+
+    SingleButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnSingleClicked);
+    LocalButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnLocalClicked);
+    OnlineButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnOnlineClicked);
+    QuitButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnQuitClicked);
+    HostButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnHostClicked);
+    JoinButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnJoinClicked);
+    BackButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnBackClicked);
+
+
+    MenuSwitcher->SetActiveWidgetIndex(0);
+
+    return true;
+
+
+}
+
+void UMainMenuWidget::OnSingleClicked() {
+
+    UGameplayStatics::OpenLevel(GetWorld(), TEXT("LVL_MAPTEST"));
+    
+}
+
+void UMainMenuWidget::OnLocalClicked() {
+
+    MenuSwitcher->SetActiveWidgetIndex(1);
+
+}
+
+void UMainMenuWidget::OnOnlineClicked() {
+
+}
+
+void UMainMenuWidget::OnQuitClicked() {
+
+        UKismetSystemLibrary::QuitGame(GetWorld(),nullptr,EQuitPreference::Quit,false);
+
+}
+
+void UMainMenuWidget::OnHostClicked() {
+
+}
+
+void UMainMenuWidget::OnJoinClicked() {
+
+}
+
+void UMainMenuWidget::OnBackClicked() {
+
+    MenuSwitcher->SetActiveWidgetIndex(0);
+
+}
--- Source/DUCKDUCK/MainMenuWidget.h
+++ Source/DUCKDUCK/MainMenuWidget.h
@@ -9,9 +9,63 @@
 /**
  * 
  */
+
+class UButton;
+class UWidgetSwitcher;
+
 UCLASS()
 class DUCKDUCK_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+
+    virtual bool Initialize() override;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* SingleButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* LocalButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* OnlineButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* HostButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* JoinButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* BackButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* QuitButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UWidgetSwitcher* MenuSwitcher;
+
+    UFUNCTION()
+    void OnSingleClicked();
+
+    UFUNCTION()
+    void OnLocalClicked();
+
+    UFUNCTION()
+    void OnOnlineClicked();
+
+    UFUNCTION()
+    void OnQuitClicked();
+
+    UFUNCTION()
+    void OnHostClicked();
+
+    UFUNCTION()
+    void OnJoinClicked();
+
+    UFUNCTION()
+    void OnBackClicked();
+
 	
 };
